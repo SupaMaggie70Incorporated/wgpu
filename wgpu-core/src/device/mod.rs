8df685ab16c2c57657dc84d4b986bed059798658
SupaMaggie70 <magnus.larsson.mn@gmail.com>
2025-11-15 01:33:51 -0600

    WHAT THE FUCK IS GOING ON

--- wgpu-core/src/device/mod.rs
+++ wgpu-core/src/device/mod.rs
@@ -518,7 +518,6 @@ pub fn create_validator(
         Caps::MESH_SHADER,
         features.intersects(wgt::Features::EXPERIMENTAL_MESH_SHADER),
     );
-    caps.set(Caps::MESH_SHADER, true);
     caps.set(
         Caps::MESH_SHADER_POINT_TOPOLOGY,
         features.intersects(wgt::Features::EXPERIMENTAL_MESH_SHADER_POINTS),
