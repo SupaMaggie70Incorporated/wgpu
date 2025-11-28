4b8fe1196b99dcc11deeae958ecb6c693d90af52
SupaMaggie70 <magnus.larsson.mn@gmail.com>
2025-11-28 01:21:38 -0600

    Thing

--- output/mesh-custom.glsl
+++ output/mesh-custom.glsl
@@ -30,14 +30,14 @@ struct MeshOutput
     uint primitive_count;
 };
 
-layout(location = 0) out _40
+out _40
 {
-    vec4 _m0;
+    layout(location = 0) vec4 _m0;
 } _43[3];
 
-layout(location = 1) perprimitiveEXT out _47
+perprimitiveEXT out _47
 {
-    vec4 _m0;
+    layout(location = 1) vec4 _m0;
 } _50[1];
 
 taskPayloadSharedEXT TaskPayload taskPayload;
@@ -46,23 +46,40 @@ shared MeshOutput mesh_output;
 
 void main()
 {
-  SetMeshOutputsEXT(3, 1);
-
-const vec4[3] positions = {vec4(0., 1.0, 0., 1.0), vec4(-1.0, -1.0, 0., 1.0),
-                           vec4(1.0, -1.0, 0., 1.0)};
-const vec4[3] colors = {vec4(0., 1., 0., 1.), vec4(0., 0., 1., 1.),
-                        vec4(1., 0., 0., 1.)};
-
-  gl_MeshVerticesEXT[0].gl_Position = vec4(0., 1.0, 0., 1.0);
-  gl_MeshVerticesEXT[1].gl_Position = vec4(-1.0, -1.0, 0., 1.0);
-  gl_MeshVerticesEXT[2].gl_Position = vec4(1.0, -1.0, 0., 1.0);
-
-  _43[0]._m0 = vec4(0., 1., 0., 1.) * taskPayload.colorMask;
-  _43[1]._m0 = vec4(0., 0., 1., 1.) * taskPayload.colorMask;
-  _43[2]._m0 = vec4(1., 0., 0., 1.) * taskPayload.colorMask;
-
-  gl_PrimitiveTriangleIndicesEXT[gl_LocalInvocationIndex] = uvec3(0, 1, 2);
-  _50[0]._m0 = vec4(1.0, 0.0, 1.0, 1.0);
-  gl_MeshPrimitivesEXT[0].gl_CullPrimitiveEXT = !taskPayload.visible;
+    if (all(equal(gl_LocalInvocationID, uvec3(0u))))
+    {
+        workgroupData = 0.0;
+        mesh_output = MeshOutput(VertexOutput[](VertexOutput(vec4(0.0), vec4(0.0)), VertexOutput(vec4(0.0), vec4(0.0)), VertexOutput(vec4(0.0), vec4(0.0))), PrimitiveOutput[](PrimitiveOutput(uvec3(0u), false, vec4(0.0))), 0u, 0u);
+    }
+    barrier();
+    mesh_output.vertex_count = 3u;
+    mesh_output.primitive_count = 1u;
+    workgroupData = 2.0;
+    mesh_output.vertices[0u].position = vec4(0.0, 1.0, 0.0, 1.0);
+    mesh_output.vertices[0u].color = vec4(0.0, 1.0, 0.0, 1.0) * taskPayload.colorMask;
+    mesh_output.vertices[1u].position = vec4(-1.0, -1.0, 0.0, 1.0);
+    mesh_output.vertices[1u].color = vec4(0.0, 0.0, 1.0, 1.0) * taskPayload.colorMask;
+    mesh_output.vertices[2u].position = vec4(1.0, -1.0, 0.0, 1.0);
+    mesh_output.vertices[2u].color = vec4(1.0, 0.0, 0.0, 1.0) * taskPayload.colorMask;
+    mesh_output.primitives[0u].indices = uvec3(0u, 1u, 2u);
+    mesh_output.primitives[0u].cull = !taskPayload.visible;
+    mesh_output.primitives[0u].colorMask = vec4(1.0, 0.0, 1.0, 1.0);
+    barrier();
+    uint _114 = min(mesh_output.vertex_count, 3u);
+    uint _117 = min(mesh_output.primitive_count, 1u);
+    SetMeshOutputsEXT(_114, _117);
+    uint _30 = gl_LocalInvocationIndex;
+    for (; _30 < _114; _30++)
+    {
+        gl_MeshVerticesEXT[_30].gl_Position = mesh_output.vertices[_30].position;
+        gl_MeshVerticesEXT[_30].gl_Position.y = -mesh_output.vertices[_30].position.y;
+        _43[_30]._m0 = mesh_output.vertices[_30].color;
+    }
+    _30 = gl_LocalInvocationIndex;
+    for (; _30 < _117; _30++)
+    {
+        gl_PrimitiveTriangleIndicesEXT[0u] = mesh_output.primitives[0u].indices;
+        gl_MeshPrimitivesEXT[0u].gl_CullPrimitiveEXT = mesh_output.primitives[0u].cull;
+        _50[0u]._m0 = mesh_output.primitives[0u].colorMask;
+    }
 }
-
