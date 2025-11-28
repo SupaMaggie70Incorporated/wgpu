c36bbed3b01527a084e67b1c67d4923c13c44fac
SupaMaggie70 <magnus.larsson.mn@gmail.com>
2025-11-28 00:57:35 -0600

    Uploaded new test shaders

--- output/mesh-custom.glsl
+++ output/mesh-custom.glsl
@@ -1,44 +1,68 @@
 #version 450
 #extension GL_EXT_mesh_shader : require
+layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
+layout(max_vertices = 3, max_primitives = 1, triangles) out;
+
+struct TaskPayload
+{
+    vec4 colorMask;
+    bool visible;
+};
+
+struct VertexOutput
+{
+    vec4 position;
+    vec4 color;
+};
+
+struct PrimitiveOutput
+{
+    uvec3 indices;
+    bool cull;
+    vec4 colorMask;
+};
+
+struct MeshOutput
+{
+    VertexOutput vertices[3];
+    PrimitiveOutput primitives[1];
+    uint vertex_count;
+    uint primitive_count;
+};
+
+out _40
+{
+    layout(location = 0) vec4 _m0;
+} _43[3];
+
+perprimitiveEXT out _47
+{
+    layout(location = 1) vec4 _m0;
+} _50[1];
+
+taskPayloadSharedEXT TaskPayload taskPayload;
+shared float workgroupData;
+shared MeshOutput mesh_output;
+
+void main()
+{
+  SetMeshOutputsEXT(3, 1);
 
 const vec4[3] positions = {vec4(0., 1.0, 0., 1.0), vec4(-1.0, -1.0, 0., 1.0),
                            vec4(1.0, -1.0, 0., 1.0)};
 const vec4[3] colors = {vec4(0., 1., 0., 1.), vec4(0., 0., 1., 1.),
                         vec4(1., 0., 0., 1.)};
 
-// This is an inefficient workgroup size.Ideally the total thread count would be
-// a multiple of 64
-layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
-struct PayloadData {
-  vec4 colorMask;
-  bool visible;
-};
-taskPayloadSharedEXT PayloadData payloadData;
+  gl_MeshVerticesEXT[0].gl_Position = vec4(0., 1.0, 0., 1.0);
+  gl_MeshVerticesEXT[1].gl_Position = vec4(-1.0, -1.0, 0., 1.0);
+  gl_MeshVerticesEXT[2].gl_Position = vec4(1.0, -1.0, 0., 1.0);
 
-out VertexOutput { layout(location = 0) vec4 color; }
-vertexOutput[];
-layout(location = 1) perprimitiveEXT out PrimitiveOutput { vec4 colorMask; }
-primitiveOutput[];
-
-layout(triangles, max_vertices = 3, max_primitives = 1) out;
-
-shared bool array[3];
-
-void main() {
-  SetMeshOutputsEXT(3, 1);
-
-  gl_MeshVerticesEXT[0].gl_Position = positions[0];
-  gl_MeshVerticesEXT[1].gl_Position = positions[1];
-  gl_MeshVerticesEXT[2].gl_Position = positions[2];
-
-  vertexOutput[0].color = colors[0] * payloadData.colorMask;
-  vertexOutput[1].color = colors[1] * payloadData.colorMask;
-  vertexOutput[2].color = colors[2] * payloadData.colorMask;
+  _43[0]._m0 = vec4(0., 1., 0., 1.) * taskPayload.colorMask;
+  _43[1]._m0 = vec4(0., 0., 1., 1.) * taskPayload.colorMask;
+  _43[2]._m0 = vec4(1., 0., 0., 1.) * taskPayload.colorMask;
 
   gl_PrimitiveTriangleIndicesEXT[gl_LocalInvocationIndex] = uvec3(0, 1, 2);
-  primitiveOutput[0].colorMask = vec4(1.0, 0.0, 1.0, 1.0);
-
-  array[0] = !payloadData.visible;
-  bool cull = array[gl_LocalInvocationIndex];
-  gl_MeshPrimitivesEXT[0].gl_CullPrimitiveEXT = cull;
+  _50[0]._m0 = vec4(1.0, 0.0, 1.0, 1.0);
+  gl_MeshPrimitivesEXT[0].gl_CullPrimitiveEXT = !taskPayload.visible;
 }
+
