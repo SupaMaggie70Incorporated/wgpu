12551f4b9012d6de9aee0057a902a3e2cd4d1cfd
SupaMaggie70 <magnus.larsson.mn@gmail.com>
2025-12-17 16:27:55 -0600

    Fixed warnings

--- naga/src/proc/mod.rs
+++ naga/src/proc/mod.rs
@@ -854,7 +854,7 @@ impl crate::Module {
 }
 
 impl crate::MeshOutputTopology {
-    pub fn to_builtin(&self) -> crate::BuiltIn {
+    pub const fn to_builtin(self) -> crate::BuiltIn {
         match self {
             Self::Points => crate::BuiltIn::PointIndex,
             Self::Lines => crate::BuiltIn::LineIndices,
