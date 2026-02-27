aefa1f841f483363a625eabf9b5b7389f63c0dd3
Inner-Daemons <magnus.larsson.mn@gmail.com>
2026-02-27 15:13:45 -0600

    Fixed thing

--- naga/src/back/hlsl/mod.rs
+++ naga/src/back/hlsl/mod.rs
@@ -813,7 +813,7 @@ pub fn supported_capabilities() -> crate::valid::Capabilities {
         | Caps::TEXTURE_EXTERNAL
         | Caps::SHADER_FLOAT16_IN_FLOAT32
         | Caps::SHADER_BARYCENTRICS
-        // No MESH_SHADER
+        | Caps::MESH_SHADER
         // No MESH_SHADER_POINT_TOPOLOGY
         | Caps::TEXTURE_AND_SAMPLER_BINDING_ARRAY_NON_UNIFORM_INDEXING
         // No BUFFER_BINDING_ARRAY_NON_UNIFORM_INDEXING
